f6f5907f76b78f428b0663c28a159acdb4a213ab
kennen <kkt0929@midasit.com>
2019-06-07 17:50:47 +0900

    Rename

--- src/main/java/com/server/config/casclienta/CasClientApplication.java
+++ src/main/java/com/server/config/casclienta/CasClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasclientaApplication {
+public class CasClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CasclientaApplication.class, args);
+        SpringApplication.run(CasClientApplication.class, args);
     }
 
 }
--- src/test/java/com/server/config/casclienta/CasClientApplicationTests.java
+++ src/test/java/com/server/config/casclienta/CasClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CasclientaApplicationTests {
+public class CasClientApplicationTests {
 
     @Test
     public void contextLoads() {
