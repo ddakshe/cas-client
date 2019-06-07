5180ab773dae20111c8ddfa6cf0c359ebb75bdb5
kennen <kkt0929@midasit.com>
2019-06-07 15:17:06 +0900

    first commit

---
+++ src/main/java/com/server/config/casclienta/CasclientaApplication.java
@@ -0,0 +1,13 @@
+package com.server.config.casclienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasclientaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasclientaApplication.class, args);
+    }
+
+}
